c718e6b1ebb79a82dae93b60869ad45b19c8db1c
andela-fojuri <fisayomi.ojuri@andela.com>
2018-07-23 17:29:17 +0100

    Update app, install dependencies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import routes from './routes/routes';
 
 dotenv.config();
 const app = express();
@@ -8,5 +9,7 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+routes(app);
 
 export default app;
