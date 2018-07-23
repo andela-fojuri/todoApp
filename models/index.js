012ca424c8399307289f71ba75bfad84562aabff
andela-fojuri <fisayomi.ojuri@andela.com>
2018-07-23 17:28:34 +0100

    Update User Model

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.js`)[env]; // eslint-disable-line
 const db = {};
 
-const sequelize = new Sequelize(config.url);
+const sequelize = new Sequelize(config.url, config);
 
 fs
   .readdirSync(__dirname)
